7c4581f2933dec87878b9a3c762db12f3674e4b4
bharath_adcrack <bharath@adcrack.com>
2024-07-02 15:18:18 +0530

    Firebase analytics unity package v12.0 added

--- Assets/FirebaseInitializer.cs
+++ Assets/FirebaseInitializer.cs
@@ -1,61 +1,88 @@
 using Firebase;
+using Firebase.Analytics;
 using Firebase.Extensions;
 using Firebase.Messaging;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FirebaseInitializer : MonoBehaviour
 {
+    [SerializeField] TMP_Text outPutStatus;
     [SerializeField] private TMP_InputField firebaseTokenOnReceivedText;
-    // [SerializeField] private TMP_InputField firebaseMessageOnReceivedText;
+    [SerializeField] string mail;
 
-    private FirebaseApp app;
 
+    private void Awake()
+    {
+        Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
+        Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
+        outPutStatus.text = "output\n";
+    }
+
+    private void OnDestroy()
+    {
+        Firebase.Messaging.FirebaseMessaging.TokenReceived -= OnTokenReceived;
+        Firebase.Messaging.FirebaseMessaging.MessageReceived -= OnMessageReceived;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
 
-        Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
-        Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
+        
         Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
         {
-            var dependencyStatus = task.Result;
-            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            // var dependencyStatus = 
+            if (task.Result == Firebase.DependencyStatus.Available)
             {
-                // Create and hold a reference to your FirebaseApp,
-                // where app is a Firebase.FirebaseApp property of your application class.
-                app = Firebase.FirebaseApp.DefaultInstance;
-
-                // Set a flag here to indicate whether Firebase is ready to use by your app.
+                InitializeAnalytics();
+                outPutStatus.text += $"<color=green>{task.Result}</color>";
             }
             else
             {
-                UnityEngine.Debug.LogError(System.String.Format(
-                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
-                // Firebase Unity SDK is not safe to use here.
+
+                outPutStatus.text += $"<color=red>{task.Result}</color>";
             }
         });
     }
 
     private void OnMessageReceived(object sender, MessageReceivedEventArgs e)
     {
-        Debug.Log($"{e.Message}");
-        // firebaseMessageOnReceivedText.text = e.Message.ToString();
+        try
+        {
+            Debug.Log($"{e.Message}");
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.Message);
+        }
+        
+
     }
 
     private void OnTokenReceived(object sender, TokenReceivedEventArgs e)
     {
-        Debug.Log($"{e.Token}");
-        firebaseTokenOnReceivedText.text = e.Token;
+        try
+        {
+            Debug.Log($"{e.Token}");
+            firebaseTokenOnReceivedText.text = e.Token;
+        }
+        catch(Exception ex)
+        {
+            Debug.Log(ex.Message);
+        }
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    void InitializeAnalytics()
     {
-
+        FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        outPutStatus.text += $"<color=green>{nameof(InitializeAnalytics)}</color>";
     }
+
+   
 }
